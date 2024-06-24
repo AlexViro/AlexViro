0de083449dca14f58c47da9ac51a9e0f6f7fea58
AlexViro <99212268+AlexViro@users.noreply.github.com>
2024-06-24 12:44:04 +0300

    Add files via upload
    Сделал адаптив страниц. Добавил страницу политики обработки персональных данных

---
+++ js/main.js
@@ -0,0 +1,45 @@
+"use strict"      
+
+const catItem = document.querySelectorAll(".catalog__block");
+const catContent = document.querySelectorAll('.catalog-cards__item');
+
+
+catItem.forEach(function(element) {
+ element.addEventListener('click', open)
+})
+
+function open(evt) {
+  const catTarget = evt.currentTarget;
+  const button = catTarget.dataset.button;
+
+  catItem.forEach(function(item){
+    item.classList.remove("catalog__block-active");
+  });
+  
+  
+  catTarget.classList.add("catalog__block-active");
+
+  catContent.forEach(function(item){
+    item.classList.remove("catalog-cards__item-active");
+  });
+
+  document.querySelector(`#${button}`).classList.add('catalog-cards__item-active');
+}
+
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle("menu__list--active");
+});
+
+const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
